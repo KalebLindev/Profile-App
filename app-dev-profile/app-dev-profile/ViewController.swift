8a45bcd4578cd82da776d6976bb76ef05229dfd4
Kaleb Lindevaldsen <kflindevaldsen@liberty.edu>
2020-08-23 15:26:39 -0400

    layout for iPhone done

---
+++ app-dev-profile/app-dev-profile/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  app-dev-profile
+//
+//  Created by Kaleb Lindevaldsen on 8/23/20.
+//  Copyright © 2020 Kaleb Lindevaldsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+
+}
+
